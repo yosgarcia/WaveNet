514976b981ed01160429d37db729a13a07ada047
mlatysh199 <maximilian.latysh@gmail.com>
2025-05-14 16:07:36 -0600

    Updated node system to be able to properly recieve messages from encrypted sources

--- WaveNetCore/WaveNetNode.py
+++ WaveNetCore/WaveNetNode.py
@@ -1,39 +1,39 @@
 from threading import Thread, Lock
-
+import time
 import hashlib
 from WaveNetCommunication import *
 
 class NodeInfo:
-	def __init__(self, ID, neighbors=set()):
+	def __init__(self, ID, private_key, neighbors=set()):
 		self.ID = ID
 		self.neighbors = neighbors
+		self.private_key = private_key
 	
 	def add_neighbor(self, link):
 		self.neighbors.add(link)
 
 class Node:
-	message_reset_time = int(5e9) # 5 segundos
-
 	def __init__(self, info, protocols, process):
 		self.info = info
 		self.protocols = {i.protocol_type: i for i in protocols}
 		self.process = process
-		self.message_queue = dict()
+		self.messages = set()
 		self.mutex = Lock()
 	
 	def listen(self):
 		for protocol_type, protocol in self.protocols: protocol.listen(self.recv)
 
-	def recv(self, packet, rlink):
+	def recv(self, packet):
 		self.mutex.acquire()
 
-		self.info.add_neighbor(rlink)
-
-		time = get_time()
 		data = packet.form()
-		if data in self.message_queue and time - self.message_queue[data] < Node.message_reset_time: return
-		self.message_queue[data] = time
-		if packet.dest == str(self.info.ID): self.process(packet.message)
+		if data in self.messages: return
+		self.messages.add(data)
+
+		if type(packet) == SecretPacket: packet = decrypt_packet(packet, self.info.private_key)
+		if type(packet) == Packet and packet.is_null(): return
+
+		if type(packet) == Packet and packet.dest == self.info.ID: self.process(packet)
 		prop(packet)
 
 		self.mutex.release()
